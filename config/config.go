58ebb7dec3c73a05057ada64eb7c2f0662d2f1a8
amikai <as23041248@gmail.com>
2019-12-07 16:36:21 +0800

    Create rough config structure
    Can load yaml config file at project folder.
    There are jwt section in our config now.

--- config/config.go
+++ config/config.go
@@ -1,1 +1,59 @@
 package config
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/spf13/viper"
+)
+
+var Conf ConfYaml
+
+var defaultConf = []byte(`
+jwt:
+	key: "YOUR_JWT_KEY"
+	age: 3000 # UNIX timestamp
+`)
+
+type ConfYaml struct {
+	JWT SectionJWT `yaml: "jwt"`
+}
+
+type SectionJWT struct {
+	Key string `yaml: "key"`
+	Age int    `yaml: "age"`
+}
+
+func LoadConf(confPath string) error {
+
+	viper.SetConfigType("yaml")
+
+	if confPath != "" {
+		content, err := ioutil.ReadFile(confPath)
+
+		if err != nil {
+			return err
+		}
+
+		if err := viper.ReadConfig(bytes.NewBuffer(content)); err != nil {
+			return err
+		}
+	} else {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("config")
+
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err == nil {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		} else {
+			// load default config
+			if err := viper.ReadConfig(bytes.NewBuffer(defaultConf)); err != nil {
+				return err
+			}
+		}
+	}
+	Conf.JWT.Key = viper.GetString("jwt.key")
+	Conf.JWT.Age = viper.GetInt("jwt.age")
+	return nil
+}
